5e73e7e3ba053c5df918a1ca80affa0d8dd393bf
Dmitry Shylov <d.shylov@leboutique.com>
2017-01-20 17:54:53 +0200

    travis

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/dshylov22/PHP.svg?branch=master)](https://travis-ci.org/dshylov22/PHP)
--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,3 @@
 <?php
-    error_reporting(E_ALL);
     define('__FIXTURES__', __DIR__ . '/fixtures');
     require_once __DIR__ . '/vendor/autoload.php';

a43dc4147c2ece7c94954130b329e541a88ed537
Dmitry Shylov <d.shylov@leboutique.com>
2017-01-20 14:45:59 +0200

    Настройки

---
+++ bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+    error_reporting(E_ALL);
+    define('__FIXTURES__', __DIR__ . '/fixtures');
+    require_once __DIR__ . '/vendor/autoload.php';
---
+++ tests/Functions/AnonymousFunctionsTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tests\Functions;
+
+use PHPUnit_Framework_TestCase;
+
+class AnonymousFunctionsTest extends PHPUnit_Framework_TestCase
+{
+
+}
